38ab667d5ebbcd22943a49cb912fb4f93a2fa1c5
Daniel Vallance <danielvallance3435@gmail.com>
2025-12-01 10:46:52 +0000

    Puzzle 1: Read and print each line of the input

--- secret_entrance_01/src/main.rs
+++ secret_entrance_01/src/main.rs
@@ -1,3 +1,20 @@
-fn main() {
-    println!("Hello, world!");
+use std::{
+    error::Error,
+    fs::File,
+    io::{self, BufRead},
+};
+
+fn main() -> Result<(), Box<dyn Error>> {
+    let mut lines = read_lines("input.txt")?;
+    while let Some(Ok(line)) = lines.next() {
+        println!("{line}");
+    }
+
+    Ok(())
+}
+
+fn read_lines(filename: &str) -> io::Result<io::Lines<io::BufReader<File>>> {
+    let file = File::open(filename)?;
+
+    Ok(io::BufReader::new(file).lines())
 }

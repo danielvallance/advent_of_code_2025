dbad9a1a9ff1b7b84277a7a201a5a102ef023de0
Daniel Vallance <danielvallance3435@gmail.com>
2025-12-01 11:14:32 +0000

    Puzzle 1: Completed part one
    This commit keeps track of the position of the dial
    after each move, and counts the number of times the dial
    hits zero (which is what the password will be)

--- secret_entrance_01/src/main.rs
+++ secret_entrance_01/src/main.rs
@@ -5,11 +5,33 @@ use std::{
 };
 
 fn main() -> Result<(), Box<dyn Error>> {
+    let mut position = 50;
+    let mut password = 0;
     let mut lines = read_lines("input.txt")?;
     while let Some(Ok(line)) = lines.next() {
-        println!("{line}");
+        let mut line = line.chars();
+
+        let dir = line.next().ok_or("Could not parse line")?;
+        let magnitude: i32 = line.collect::<String>().parse()?;
+
+        match dir {
+            'R' => position += magnitude,
+            'L' => position -= magnitude,
+            _ => return Err("Invalid direction".into()),
+        }
+
+        position %= 100;
+
+        while position < 0 {
+            position += 100;
+        }
+
+        if position == 0 {
+            password += 1;
+        }
     }
 
+    println!("The password is {password}");
     Ok(())
 }
 

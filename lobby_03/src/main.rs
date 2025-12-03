b69661a73173d80478ad680b36df57226cbcaf3a
Daniel Vallance <danielvallance3435@gmail.com>
2025-12-03 19:06:24 +0000

    Puzzle 3: Read lines from input file

--- lobby_03/src/main.rs
+++ lobby_03/src/main.rs
@@ -1,3 +1,24 @@
-fn main() {
-    println!("Hello, world!");
+use std::{
+    error::Error,
+    fs::File,
+    io::{self, BufRead},
+    path::Path,
+};
+
+fn main() -> Result<(), Box<dyn Error>> {
+    let mut lines = read_lines("input.txt")?;
+    while let Some(Ok(line)) = lines.next() {
+        println!("{line}");
+    }
+
+    Ok(())
+}
+
+fn read_lines<T>(filepath: T) -> Result<io::Lines<io::BufReader<File>>, Box<dyn Error>>
+where
+    T: AsRef<Path>,
+{
+    let file = File::open(filepath)?;
+
+    Ok(io::BufReader::new(file).lines())
 }

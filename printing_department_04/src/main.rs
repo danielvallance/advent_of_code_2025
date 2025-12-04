d9db19dcde5984db79b2e2462d0c1b67dc28ddf3
Daniel Vallance <danielvallance3435@gmail.com>
2025-12-04 22:14:27 +0000

    Puzzle 4: completed part 1
    Convert the input to a vector of prefix lists, making it easier
    to calculate the number of surrounding rolls

--- printing_department_04/src/main.rs
+++ printing_department_04/src/main.rs
@@ -6,11 +6,51 @@ use std::{
 };
 
 fn main() -> Result<(), Box<dyn Error>> {
-    let mut lines = read_lines("input.txt")?;
-    while let Some(Ok(line)) = lines.next() {
-        println!("{line}");
+    let grid: Vec<Vec<i32>> = read_lines("input.txt")?
+        .map(|line| {
+            let line = line?;
+            Ok(line.chars().map(|c| if c == '@' { 1 } else { 0 }).collect())
+        })
+        .collect::<Result<_, Box<dyn Error>>>()?;
+
+    let prefix_lists: Vec<Vec<i32>> = grid
+        .iter()
+        .map(|row| {
+            row.iter()
+                .scan(0, |state, &cell| {
+                    *state += cell;
+                    Some(*state)
+                })
+                .collect()
+        })
+        .collect();
+
+    let mut answer = 0;
+
+    for row in 0..grid.len() {
+        for col in 0..grid[0].len() {
+            if grid[row][col] != 1 {
+                continue;
+            }
+            let mut surrounding_rolls = -1;
+
+            surrounding_rolls += (row.saturating_sub(1)..=(row + 1).min(grid.len() - 1))
+                .map(|idx| {
+                    let mut value = prefix_lists[idx][(col + 1).min(grid[0].len() - 1)];
+                    if col > 1 {
+                        value -= prefix_lists[idx][col - 2];
+                    }
+                    value
+                })
+                .sum::<i32>();
+
+            if surrounding_rolls < 4 {
+                answer += 1;
+            }
+        }
     }
 
+    println!("The answer is {answer}");
     Ok(())
 }
 

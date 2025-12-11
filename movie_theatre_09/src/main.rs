ac8cc25f1dfe6248e657cbf9bd81f23e4a077f3c
Daniel Vallance <danielvallance3435@gmail.com>
2025-12-11 21:33:38 +0000

    Puzzle 9: Completed part 1

--- movie_theatre_09/src/main.rs
+++ movie_theatre_09/src/main.rs
@@ -5,13 +5,62 @@ use std::{
     path::Path,
 };
 
+#[derive(Debug, PartialEq)]
+enum MyError {
+    InvalidFileFormat,
+}
+
+impl std::fmt::Display for MyError {
+    fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
+        write!(f, "Invalid file format")
+    }
+}
+
+impl Error for MyError {}
+
+#[derive(Debug, PartialEq)]
+struct Point {
+    x: u64,
+    y: u64,
+}
+
+impl Point {
+    fn new(x: u64, y: u64) -> Self {
+        Point { x, y }
+    }
+
+    fn area(&self, other: &Point) -> u64 {
+        let x_len = self.x.abs_diff(other.x) + 1;
+        let y_len = self.y.abs_diff(other.y) + 1;
+        x_len * y_len
+    }
+}
+
 fn main() -> Result<(), Box<dyn Error>> {
     let lines = read_lines("input.txt")?;
 
-    for line in lines {
-        let line = line?;
-        println!("{line}");
-    }
+    let points = lines
+        .map(|line| {
+            let mut coords = line?
+                .split(',')
+                .map(|coord| Ok(coord.parse::<u64>()?))
+                .collect::<Result<Vec<u64>, Box<dyn Error>>>()?
+                .into_iter();
+
+            Ok(Point::new(
+                coords.next().ok_or(MyError::InvalidFileFormat)?,
+                coords.next().ok_or(MyError::InvalidFileFormat)?,
+            ))
+        })
+        .collect::<Result<Vec<Point>, Box<dyn Error>>>()?;
+
+    let answer = points
+        .iter()
+        .flat_map(|point_a| points.iter().map(|point_b| point_a.area(point_b)))
+        .max()
+        .unwrap_or(0);
+
+    println!("The answer is {answer}");
 
     Ok(())
 }

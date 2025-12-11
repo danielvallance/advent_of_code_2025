4f6566cd3ecc40b9a16add1607006647002db0a7
Daniel Vallance <danielvallance3435@gmail.com>
2025-12-11 15:35:42 +0000

    Puzzle 7: Completed part 1

--- laboratories_07/src/main.rs
+++ laboratories_07/src/main.rs
@@ -1,4 +1,5 @@
 use std::{
+    collections::HashSet,
     error::Error,
     fs::File,
     io::{self, BufRead, BufReader, Lines},
@@ -8,10 +9,32 @@ use std::{
 fn main() -> Result<(), Box<dyn Error>> {
     let lines = read_lines("input.txt")?;
 
+    let mut beam_pos = HashSet::new();
+
+    let mut answer = 0;
+
     for line in lines {
-        let line = line?;
-        println!("{line}");
+        let line_chars = line?.chars().collect::<Vec<char>>();
+
+        if beam_pos.is_empty() {
+            if let Some(initial_beam_pos) = line_chars.iter().position(|&c| c == 'S') {
+                beam_pos.insert(initial_beam_pos);
+            }
+        } else {
+            let mut new_beam_pos = HashSet::new();
+            for (idx, &c) in line_chars.iter().enumerate() {
+                if c == '^' && beam_pos.contains(&idx) {
+                    answer += 1;
+                    beam_pos.remove(&idx);
+                    new_beam_pos.insert(idx - 1);
+                    new_beam_pos.insert(idx + 1);
+                }
+            }
+            beam_pos.extend(new_beam_pos);
+        }
     }
+
+    println!("The answer is {answer}");
 
     Ok(())
 }

66c2dddef0ab9d506cb6a31d74d4ca1024e3010a
Daniel Vallance <danielvallance3435@gmail.com>
2025-12-11 10:54:20 +0000

    Puzzle 6: Completed part 1
    Parsed the sums, calculated the answers, and found the overall
    answer by summing those answers.

--- trash_compactor_06/src/main.rs
+++ trash_compactor_06/src/main.rs
@@ -5,13 +5,51 @@ use std::{
     path::Path,
 };
 
-fn main() -> Result<(), Box<dyn Error>> {
-    let lines = read_lines("input.txt")?;
+const INVALID_FILE_FORMAT: &str = "Invalid file format";
 
-    for line in lines {
-        let line = line?;
-        println!("{line}");
+enum OP {
+    ADD,
+    MUL,
+}
+
+fn str_to_op(s: &str) -> Result<OP, Box<dyn Error>> {
+    match s {
+        "+" => Ok(OP::ADD),
+        "*" => Ok(OP::MUL),
+        _ => Err(INVALID_FILE_FORMAT.into()),
     }
+}
+
+fn main() -> Result<(), Box<dyn Error>> {
+    let mut elements = read_lines("input.txt")?
+        .map(|line| {
+            let line = line?;
+            Ok(line.split_whitespace().map(|s| s.to_owned()).collect())
+        })
+        .collect::<Result<Vec<Vec<String>>, Box<dyn Error>>>()?;
+
+    let operators = elements.pop().ok_or(INVALID_FILE_FORMAT)?;
+
+    let operands = elements;
+
+    let mut answer = 0;
+
+    for idx in 0..operands[0].len() {
+        let op_str = operators.get(idx).ok_or(INVALID_FILE_FORMAT)?;
+        let op = str_to_op(op_str)?;
+
+        let current_operands = operands
+            .iter()
+            .map(|operands| Ok(operands[idx].parse::<i64>()?))
+            .collect::<Result<Vec<i64>, Box<dyn Error>>>()?;
+
+        answer += match op {
+            OP::ADD => current_operands.iter().sum::<i64>(),
+            OP::MUL => current_operands.into_iter().product::<i64>(),
+        }
+    }
+
+    println!("The answer is {answer}");
 
     Ok(())
 }

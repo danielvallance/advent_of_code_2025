fcabc0fa9fac50417db753d2bdcc7e29f19db390
Daniel Vallance <danielvallance3435@gmail.com>
2025-12-11 17:13:11 +0000

    Puzzle 8: Parse lines as coordinates and print them

--- playground_08/src/main.rs
+++ playground_08/src/main.rs
@@ -5,13 +5,51 @@ use std::{
     path::Path,
 };
 
+#[derive(Debug, PartialEq)]
+enum MyError {
+    InvalidFileFormat,
+}
+
+impl std::fmt::Display for MyError {
+    fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
+        write!(f, "Invalid file format")
+    }
+}
+impl std::error::Error for MyError {}
+
+#[derive(Debug)]
+struct Point {
+    x: u32,
+    y: u32,
+    z: u32,
+}
+
+impl Point {
+    fn new(x: u32, y: u32, z: u32) -> Point {
+        Point { x, y, z }
+    }
+}
+
 fn main() -> Result<(), Box<dyn Error>> {
     let lines = read_lines("input.txt")?;
 
-    for line in lines {
-        let line = line?;
-        println!("{line}");
-    }
+    let points = lines
+        .into_iter()
+        .map(|line| {
+            let mut coords = line?
+                .split(',')
+                .map(|coord_str| Ok(coord_str.parse::<u32>()?))
+                .collect::<Result<Vec<u32>, Box<dyn Error>>>()?
+                .into_iter();
+            Ok(Point::new(
+                coords.next().ok_or(MyError::InvalidFileFormat)?,
+                coords.next().ok_or(MyError::InvalidFileFormat)?,
+                coords.next().ok_or(MyError::InvalidFileFormat)?,
+            ))
+        })
+        .collect::<Result<Vec<Point>, Box<dyn Error>>>()?;
+
+    println!("{points:?}");
 
     Ok(())
 }

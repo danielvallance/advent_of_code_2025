35aad1bad7aa1b0e2fff15a10cf1c66c23f974cf
Daniel Vallance <danielvallance3435@gmail.com>
2025-12-12 11:58:21 +0000

    Puzzle 9: Completed part 2
    Filtered invalid rectangles which either had an edge crossing
    through the middle, or were entirely outside the red/green tiles

--- movie_theatre_09/src/main.rs
+++ movie_theatre_09/src/main.rs
@@ -36,6 +36,70 @@ impl Point {
     }
 }
 
+fn is_valid_rectangle(point_a: &Point, point_b: &Point, edges: &[(Point, Point)]) -> bool {
+    let (low_x_rect, high_x_rect) = if point_a.x < point_b.x {
+        (point_a.x, point_b.x)
+    } else {
+        (point_b.x, point_a.x)
+    };
+    let (low_y_rect, high_y_rect) = if point_a.y < point_b.y {
+        (point_a.y, point_b.y)
+    } else {
+        (point_b.y, point_a.y)
+    };
+
+    for (e1, e2) in edges {
+        let x1_edge = e1.x;
+        let x2_edge = e2.x;
+        let y1_edge = e1.y;
+        let y2_edge = e2.y;
+
+        if x1_edge == x2_edge {
+            let (low_y_edge, high_y_edge) = if y1_edge < y2_edge {
+                (y1_edge, y2_edge)
+            } else {
+                (y2_edge, y1_edge)
+            };
+
+            if low_x_rect < x1_edge
+                && x1_edge < high_x_rect
+                && low_y_edge.max(low_y_rect) < high_y_edge.min(high_y_rect)
+            {
+                return false;
+            }
+        } else {
+            let (low_x_edge, high_x_edge) = if x1_edge < x2_edge {
+                (x1_edge, x2_edge)
+            } else {
+                (x2_edge, x1_edge)
+            };
+
+            if low_y_rect < y1_edge
+                && y1_edge < high_y_rect
+                && low_x_edge.max(low_x_rect) < high_x_edge.min(high_x_rect)
+            {
+                return false;
+            }
+        }
+    }
+
+    let mid_x_rect = low_x_rect + (high_x_rect - low_x_rect) / 2;
+    let mid_y_rect = low_y_rect + (high_y_rect - low_y_rect) / 2;
+
+    let mut crossings = 0;
+    for (e1, e2) in edges {
+        if e1.x == e2.x
+            && e1.y.min(e2.y) < mid_y_rect
+            && mid_y_rect < e1.y.max(e2.y)
+            && e1.x > mid_x_rect
+        {
+            crossings += 1;
+        }
+    }
+
+    crossings % 2 == 1
+}
+
 fn main() -> Result<(), Box<dyn Error>> {
     let lines = read_lines("input.txt")?;
 
@@ -62,7 +126,12 @@ fn main() -> Result<(), Box<dyn Error>> {
 
     let answer = points
         .iter()
-        .flat_map(|point_a| points.iter().map(|point_b| point_a.area(point_b)))
+        .flat_map(|point_a| {
+            points
+                .iter()
+                .filter(|&point_b| is_valid_rectangle(point_a, point_b, &edges))
+                .map(|point_b| point_a.area(point_b))
+        })
         .max()
         .unwrap_or(0);
 

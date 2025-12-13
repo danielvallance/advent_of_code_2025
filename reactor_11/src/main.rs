6c550f38a2261d70c7cd8014f670201c12f91a48
Daniel Vallance <danielvallance3435@gmail.com>
2025-12-13 20:01:58 +0000

    Puzzle 11: Completed part 1
    Used a BFS to solve this

--- reactor_11/src/main.rs
+++ reactor_11/src/main.rs
@@ -1,17 +1,63 @@
 use std::{
+    collections::{HashMap, VecDeque},
     error::Error,
     fs::File,
     io::{self, BufRead, BufReader, Lines},
     path::Path,
 };
 
+#[derive(Debug)]
+enum MyError {
+    InvalidFileFormat,
+}
+
+impl std::fmt::Display for MyError {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "Invalid file format")
+    }
+}
+
+impl std::error::Error for MyError {}
+
 fn main() -> Result<(), Box<dyn Error>> {
     let lines = read_lines("input.txt")?;
 
+    let mut adjacencies = HashMap::new();
+
     for line in lines {
         let line = line?;
-        println!("{line}");
+
+        let mut devices = line
+            .split_whitespace()
+            .map(|s| s.to_owned())
+            .collect::<VecDeque<String>>();
+
+        let start = devices
+            .pop_front()
+            .ok_or(MyError::InvalidFileFormat)?
+            .replace(":", "");
+
+        adjacencies.entry(start).or_insert(vec![]).extend(devices);
     }
+
+    let mut frontier = VecDeque::new();
+
+    frontier.push_back("you");
+
+    let mut answer = 0;
+
+    while let Some(cur) = frontier.pop_front() {
+        if cur == "out" {
+            answer += 1;
+            continue;
+        }
+
+        for next in adjacencies.get(cur).ok_or(MyError::InvalidFileFormat)? {
+            frontier.push_back(next);
+        }
+    }
+
+    println!("The answer is {answer}");
 
     Ok(())
 }

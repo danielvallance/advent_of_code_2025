3b2f3a73b8a77bebca8efe094e838b7232b57a33
Daniel Vallance <danielvallance3435@gmail.com>
2025-12-14 12:32:29 +0000

    Puzzle 12: Read and print lines from input file

--- christmas_tree_farm_12/src/main.rs
+++ christmas_tree_farm_12/src/main.rs
@@ -1,3 +1,25 @@
-fn main() {
-    println!("Hello, world!");
+use std::{
+    error::Error,
+    fs::File,
+    io::{self, BufRead, BufReader, Lines},
+    path::Path,
+};
+
+fn main() -> Result<(), Box<dyn Error>> {
+    let lines = read_lines("input.txt")?;
+
+    for line in lines {
+        let line = line?;
+        println!("{line}");
+    }
+
+    Ok(())
+}
+
+fn read_lines<T>(filename: T) -> io::Result<Lines<BufReader<File>>>
+where
+    T: AsRef<Path>,
+{
+    let file = File::open(filename)?;
+    Ok(BufReader::new(file).lines())
 }
